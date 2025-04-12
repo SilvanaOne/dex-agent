8ada90653e8e828edcb4c8d1af178f2723fa662f
DFST <mk@dfst.io>
2025-04-13 00:18:13 +0300

    move prisma back to lib

--- packages/ui/src/components/dex/sql.tsx
+++ packages/ui/src/components/dex/sql.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { sqlReadOnlyQuery } from "@/lib/sql";
+import { sqlReadOnlyQuery } from "@dex-agent/lib";
 interface SqlQueryProps {
   blockNumber: number;
   sequence: number;
